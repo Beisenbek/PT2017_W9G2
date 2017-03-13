8094e40e79d9f38c9b57bd5b30bdaef4611a7a96
beisenbek.baisakov <beisenbek.baisakov@yandex.ru>
2017-03-13 12:16:05 +0600

    init

---
+++ CalculatorApp/CaclBrain.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorApp
+{
+    public class CaclBrain
+    {
+        public double firstoperand;
+        public double secondoperand;
+        public double result;
+
+        public double Plus()
+        {
+            result = firstoperand + secondoperand;
+            return result;
+        }
+
+    }
+}
---
+++ CalculatorApp/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorApp
+{
+    public partial class Form1 : Form
+    {
+        CaclBrain brain = new CaclBrain();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void digitbtn_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            display.Text += btn.Text;
+        }
+
+        private void operationbtn_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+           if(btn.Text == "+")
+            {
+                brain.firstoperand = double.Parse( display.Text);
+                display.Text = "";
+            }
+            else if (btn.Text == "=")
+            {
+                brain.secondoperand = double.Parse(display.Text);
+                display.Text = brain.Plus().ToString();
+            }
+        }
+    }
+}
